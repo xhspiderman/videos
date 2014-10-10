68b29658deaf4534c23444c4e6127e793cf5c69c
xhspiderman <xhspiderman@gmail.com>
2014-10-10 00:34:38 -1000

    production ready code

--- videos/videos/settings.py
+++ videos/videos/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 print BASE_DIR
-SITE_HOST = '127.0.0.1:8000'
+SITE_HOST = '127.0.0.1:8000' #this needs to be changed according to production environment
 DOC_ROOT = ''
 
 # Quick-start development settings - unsuitable for production
